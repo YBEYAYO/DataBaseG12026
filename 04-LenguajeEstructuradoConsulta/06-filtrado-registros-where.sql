9c176b59bff432307205639752bf45ea1c63cf25
YBEYAYO <blackyael31@gmail.com>
2026-08-12 12:56:38 -0600

    Operadores IS NULL, IS NOT NULL, BETWEEN, NOT BETWEEN, IN Y NOT IN terminados

--- 04-LenguajeEstructuradoConsulta/06-filtrado-registros-where.sql
+++ 04-LenguajeEstructuradoConsulta/06-filtrado-registros-where.sql
@@ -213,5 +213,230 @@ WHERE v.fecha < '2025-02-01'
 -- del inventario, donde el valor del inventario debe ser mayor a 50000
 
 SELECT 
-	p.codigo AS codigo_rp
+	p.codigo AS codigo_rp44
 FROM productos AS p;
+
+
+
+
+
+
+--Seleccionar los datos de los productos Mostrando el codif¿go y el valor del inventario 
+--done el alor del inventario deve de ser mayor a 50 mill
+
+SELECT 
+p.codigo AS codigo_producto,
+p.existencia AS existencia,
+p.precio AS precio,
+(p.precio*p.existencia) AS valor_inventario
+FROM productos AS p
+WHERE (p.precio*p.existencia) > 50000;
+
+--Mostrar los productos con precio que este entre $200 y $300  
+SELECT 
+p.codigo AS codigo_producto,
+p.existencia AS existencia,
+p.precio AS precio,
+(p.precio*p.existencia) AS valor_inventario
+FROM productos AS p
+WHERE precio>=200 AND precio<=300;
+
+--Instruccion between 
+SELECT 
+p.codigo AS codigo_producto,
+p.existencia AS existencia,
+p.precio AS precio,
+(p.precio*p.existencia) AS valor_inventario
+FROM productos AS p
+WHERE precio BETWEEN 200 AND 300;
+
+
+-- los purdcutos entre 200 y 300 ademas que tengan 300 unidades
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.precio>=200
+AND 
+p.precio<=300
+AND 
+p.existencia<50
+ORDER BY precio DESC;
+
+--Mostrar los prodcutos conn existencia inferori a 10 o superorir a 190
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.existencia<=10
+OR
+p.existencia>=190
+
+ORDER BY precio DESC;
+
+--Operardor Not 
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.existencia < 10
+OR p.existencia>190;
+
+--OPERADOR NOT 
+--Mostrar los productos que su precio no sea mayor a 400no sean mayores a 400 
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.precio <= 400;
+
+--Mostrar los empleados de los departamentos 1 y 2 que tengan salario mayor 
+-- a 25000 pesos
+
+SELECT 
+e.id_empleado,
+e.nombre,
+e.salario,
+e.id_departamento
+FROM empleados AS e
+WHERE e.id_empleado = 1
+	OR 
+	e.id_departamento = 2
+	AND e.salario  > 25000;
+
+SELECT 
+e.id_empleado,
+e.nombre,
+e.salario,
+e.id_departamento
+FROM empleados AS e
+WHERE (e.id_empleado = 1
+	OR 
+	e.id_departamento = 2)
+	AND e.salario  > 25000;
+
+-- Mostrar los empleado que no tiene jefe
+SELECT
+	p.id_empleado,
+	p.id_departamento,
+	p.nombre,
+	p.id_jefe
+FROM empleados AS p
+WHERE p.id_jefe IS NULL;
+
+-- para mostrar los que si tienen jefe
+SELECT
+	p.id_empleado,
+	p.id_departamento,
+	p.nombre,
+	p.id_jefe
+FROM empleados AS p
+WHERE p.id_jefe IS NOT NULL;
+
+-- OPERADOR BETWWEN
+--Permite comprobar si un valor se encuentra dentro de un rango inclusivo
+
+/*========================================================================================
+WHERE columna WETWWEN limite_inferior AND limite_superior
+========================================================================================*/
+
+-- Mostrar empleados con salario entre $15,000 y $20,000, incluyendo ambos limites
+SELECT
+	e.id_empleado,
+	e.nombre,
+	e.salario
+FROM empleados AS e
+WHERE salario BETWEEN 15000 AND 20000
+ORDER BY 3 DESC;
+
+-- con un AND
+SELECT
+	e.id_empleado,
+	e.nombre,
+	e.salario
+FROM empleados AS e
+WHERE e.salario >= 15000
+	AND
+	e.salario <= 20000;
+
+-- Mostrar los productos que los precios esten en el raqngo de 100 y 200
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE P.precio BETWEEN 100 AND 200;
+
+-- Mostrar las ventas del 1 de enero de 2025 al 10 de enero de 2025
+SELECT 
+	v.id_venta AS [numero_ventas],
+	v.id_cliente AS [cliente],
+	v.id_empleado AS [vendedeor],
+	v.fecha [fecha_venta],
+	UPPER(FORMAT(v.fecha, 'MMMM', 'es-ES')) AS [mes_venta],
+	UPPER(FORMAT(v.fecha, 'dddd', 'es-ES')) AS [dia_venta],
+	DATEPART(YEAR, v.fecha) AS [año-venta]
+FROM ventas AS v
+WHERE v.fecha BETWEEN '2025-01-01' AND '2025-01-10'
+ORDER BY [cliente] ASC;
+
+-- Mostrar los productos que su rango de precios no este entre 100 y 400 
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.precio NOT BETWEEN 100 AND 400;
+
+-- OPERQADOR IN
+
+
+-- Permite comparar una columna con una lista de valores
+
+/*========================================================================================
+WHERE columna IN (valor_1, valor_2, valor_3)
+========================================================================================*/
+-- Mostrar productos pertenecientes a las categorias 1, 7, 12
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.id_categoria IN (1,7,12)
+ORDER BY p.id_categoria;
+
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.id_categoria = 1
+	OR p.id_categoria = 7
+	OR p.id_categoria = 12
+
+ORDER BY p.id_categoria;
+
+-- Mostrar todos los productos que no pertenecen a la categoria 1, 7 o 12
+
+SELECT
+ p.codigo,
+ p.nombre,
+ p.precio,
+ p.existencia
+FROM productos AS p
+WHERE p.id_categoria NOT IN (1,7,12)
+ORDER BY p.id_categoria;
+
+-- TODO: OPERADOR LIKE
